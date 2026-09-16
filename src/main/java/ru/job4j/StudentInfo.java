98fcda162f128cd617d608eea613995e2d6daf82
baguwixaezakmi <baguwixaezakmi@gmail.com>
2026-09-16 08:07:30 +0300

    4.1. Разбор первой программы.

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("David Khamzin");
+        System.out.println("25.04.2004");
+    }
+}
